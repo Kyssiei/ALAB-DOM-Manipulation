839a0f706a9591ad6870e37ec7a88e77c09cfb4d
Kyssiei <Kyssiei.kay@gmail.com>
2025-01-31 20:50:05 -0500

    Updated: Pt. 2

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-const mainEL = document.querySelector("Main")
+//**================= PART 1 ========================== */
+
+const mainEL = document.querySelector("main")
 
 mainEL.style.backgroundColor = "var(--main-bg)"
 
 mainEL.innerHTML = "<h1>DOM Manipulation</h1>"
+
+mainEL.classList.add("flex-ctr")
+
+
+//**================ PART 2 =========================== */
+
+const topMenuEl = document.querySelector("#top-menu")
+
+topMenuEl.style.height = "100px"
+
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)"
+
+topMenuEl.classList.add("flex-around")
+
+
+
 
