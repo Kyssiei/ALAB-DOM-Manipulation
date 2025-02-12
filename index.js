b1a700df355f5ba966cb833d10c3be088e912b71
Kayla Gentry <kaylagentry@Kaylas-MacBook-Pro.local>
2025-02-12 14:48:53 -0500

    part 4

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
+
 // Menu data structure
 var menuLinks = [
-    { text: 'about', href: '/about' },
-    { text: 'catalog', href: '/catalog' },
-    { text: 'orders', href: '/orders' },
-    { text: 'account', href: '/account' },
-];
-
+    {text: 'about', href: '/about'},
+    {text: 'catalog', href: '#', subLinks: [
+      {text: 'all', href: '/catalog/all'},
+      {text: 'top selling', href: '/catalog/top'},
+      {text: 'search', href: '/catalog/search'},
+    ]},
+    {text: 'orders', href: '#' , subLinks: [
+      {text: 'new', href: '/orders/new'},
+      {text: 'pending', href: '/orders/pending'},
+      {text: 'history', href: '/orders/history'},
+    ]},
+    {text: 'account', href: '#', subLinks: [
+      {text: 'profile', href: '/account/profile'},
+      {text: 'sign out', href: '/account/signout'},
+    ]},
+  ];
 
 //**================= PART 1 ========================== */
 
@@ -40,4 +51,54 @@ menuLinks.forEach(link => {
     newLink.textContent = link.text;
 
     topMenuEl.appendChild(newLink);
+});
+
+
+//!================== PART 2 ===============================
+
+//**================= PART 3 =============================== */
+
+const subMenuEl = document.querySelector("#sub-menu")
+
+subMenuEl.style.height = "100%"
+
+subMenuEl.style.backgroundColor = "var(--sub-menu-bg)"
+
+subMenuEl.classList.add("flex-around")
+
+subMenuEl.style.position = "absolute"
+
+subMenuEl.style.top = "0"
+
+console.log(subMenuEl);
+
+
+//**===================== PART 4 ========================== */
+
+// //Todo: update the menu links 
+//select all <a> elements inside topModelEl
+const topMenuLinks = topMenuEl.querySelectorAll("a");
+
+// track the activivty of menu item
+let activeMenuItem = null;
+
+// Add event listener for click
+topMenuEl.addEventListener("click", function(event){
+    event.preventDefault(); // prevent defult behavior
+
+    if (event.target.tagName !== "A") return; //Ignore non <a> clicks
+
+    const clickedText = event.target.textContent.toLowerCase(); // log the text content
+    console.log(clickedText); //Log the clicked menu item 
+
+    
+    //Remove "active" from all <a> elements
+    topMenuLinks.forEach(link => link.classList.remove("active"));
+
+    //Toggle "active" on clicked link 
+    event.target.classList.add("active");
+})
+
+subMenuEl.addEventListener("click", function(event){
+    console.log("Submenu clicked:", event.target.textContent);
 });
